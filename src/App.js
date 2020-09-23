b50ca0ee3310aad04f36bf61f1f246395f3b75c3
Tushar Shinde <66311763+tusharshinde3196@users.noreply.github.com>
2020-09-23 15:00:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AutoCompletedText from './AutoCompletedText';
+
+function App() {
+  return (
+    <div className="App">
+     <AutoCompletedText/>
+    </div>
+  );
+}
+
+export default App;
